654cfcdb6402a0ee9891a7fa50e1d24a217f3d0c
ixon7 <55660649+ixon7@users.noreply.github.com>
2019-10-20 15:56:13 +0200

    Dodaj pliki projektów.

---
+++ Nemo4.0/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Nemo4._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
